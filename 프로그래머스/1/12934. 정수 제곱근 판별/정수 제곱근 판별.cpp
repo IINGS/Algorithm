14bd5760c721175ff0d0d529dcde67ff03f0e77f
IINGS <142036336+IINGS@users.noreply.github.com>
2024-12-26 16:08:29 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.01 ms, Memory: 3.63 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.cpp
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.cpp
@@ -5,15 +5,7 @@
 using namespace std;
 
 long long solution(long long n) {
-    long long answer = -1;
     long long x = sqrt(abs(n));
     
-    if (x*x == n)
-    {
-        answer = (x+1)*(x+1);
-    }else if ((x+1)*(x+1) == n)
-    {
-        answer = (x+2)*(x+2);
-    }
-    return answer;
+    return (x*x == n) ? (x+1)*(x+1): -1;
 }
