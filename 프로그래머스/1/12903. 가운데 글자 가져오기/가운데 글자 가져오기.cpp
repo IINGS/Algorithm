e5e3a9a0386e2505dabbe4e1c34376708500bab7
IINGS <142036336+IINGS@users.noreply.github.com>
2024-12-31 11:19:25 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.cpp
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.cpp
@@ -7,11 +7,11 @@ string solution(string s) {
     string answer = "";
     if (s.size()%2==0)
     {
-        answer = s.substr(s.size()/2-1,2);
-    }
-    else if (s.size()%2==1)
+        answer.push_back(s[s.size()/2-1]);
+        answer.push_back(s[s.size()/2]);
+    }else
     {
-        answer = s.substr(s.size()/2,1);
-    }else return 0;
+        answer.push_back(s[s.size()/2]);
+    }
     return answer;
 }
