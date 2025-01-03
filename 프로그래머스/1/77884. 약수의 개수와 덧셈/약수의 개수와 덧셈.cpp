c7f11d6ee701d8207212c719e5ceeaa315414097
IINGS <142036336+IINGS@users.noreply.github.com>
2025-01-03 09:23:33 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 1.16 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.cpp
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.cpp
@@ -5,20 +5,19 @@ using namespace std;
 
 int solution(int left, int right) {
     int answer = 0;
-    int det = 0;
-    int i = 1;
-    for (;left <= right; left++)
+    int num;
+    while(left<=right)
     {
-        for (; i <= left; i++)
+        for (int i=1; i<=left; i++)
         {
-            if (left % i == 0)
+            if (left%i==0)
             {
-                det += 1;
+                num+=1;
             }
         }
-        answer += det % 2 == 0 ? left : -left;
-        det = 0;
-        i = 1;
+        answer += (num%2==0)?left:-left;
+        num=0;
+        left++;
     }
     return answer;
 }
