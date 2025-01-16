a280e332af77ee8d3e51701df70f73b6ae695bab
IINGS <142036336+IINGS@users.noreply.github.com>
2025-01-16 10:08:12 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.03 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string s) {
+    string num[]= {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+    for(int i=0; i<10; i++)
+    {
+        while(s.find(num[i]) != string::npos)
+        {
+            s.replace(s.find(num[i]), num[i].size(), to_string(i));
+        }
+    }
+    return stoi(s);
+}
