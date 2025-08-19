e722c7043d11398af0de17c99dddc35f154cbd57
IINGS <142036336+IINGS@users.noreply.github.com>
2025-08-19 19:39:13 +0900

    [Bronze V] Title: A+B - 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int times;
+    cin >> times;
+    while(times--)
+    {
+        int a, b;
+        cin >> a >> b;
+        cout << a + b << endl;
+    }
+    return 0;
+}
