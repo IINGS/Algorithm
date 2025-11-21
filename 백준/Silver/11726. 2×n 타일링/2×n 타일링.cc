0c857e06eced25f522c29bf999dcdabacacdc9d0
IINGS <142036336+IINGS@users.noreply.github.com>
2025-11-22 00:21:27 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    
+    vector<int> dp(n+1);
+    dp[0]=1;
+    dp[1]=1;
+    
+    for(int i=2;i<=n;i++)
+    {
+        dp[i] = dp[i-1] % 10007 + dp[i-2] % 10007;
+    }
+    
+    int answer = dp[n] % 10007;
+    
+    cout << answer;
+    
+    return 0;
+}
